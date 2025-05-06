e3830a6c1beed1fef675ef97efb5621e7b44c8a1
Evan Le <evan@Evans-MacBook-Pro.local>
2025-05-06 16:57:35 +0700

    feat: initalize gateway and eureka server

---
+++ eureka/src/test/java/com/streaming/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streaming.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
